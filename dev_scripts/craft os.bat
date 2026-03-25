d590288c5396d478502e6790aca885c99a33ee8c
bluescreen <olivierus303@gmail.com>
2026-03-25 18:10:28 +0100

    changed the scripts

---
+++ dev_scripts/craft os.bat
@@ -0,0 +1,8 @@
+
+del %appdata%\CraftOS-PC\computers\0\* /s /q
+xcopy /s /e /h /y ..\src\* %appdata%\CraftOS-PC\computers\0\*
+call CraftOS-PC.exe
+cls
+echo press enter to delete the files from emulated computer with ID 0
+pause>nul
+del %appdata%\CraftOS-PC\computers\0\* /s /q
---
+++ dev_scripts/craft os.sh
@@ -0,0 +1,6 @@
+clear
+rm -rf ../computers
+mkdir ../computers ../computers/0
+cp src/* ../computers/0/ -r
+craftos --start-dir ../computers/0
+rm -rf ../computers
